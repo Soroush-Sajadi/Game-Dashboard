1d94580d4d63908cadea268304cf36f14b9516b5
Soroush-Sajadi <soorooshsadjadee@gmail.com>
2020-06-11 20:03:28 +0200

    finaly

--- Client/app/src/Components/Quiz.js
+++ Client/app/src/Components/Quiz.js
@@ -5,15 +5,22 @@ function Quiz ({ sideBarState, category }) {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState('Loading');
   let [nextQuestion, setNextQuestion] = useState(0)
+  let [ correctAnswer, setCorrectAnswer ] = useState(0);
 
   
   const getNextQuestion = async evt => {
     evt.preventDefault();
-
-    console.log(evt.target.value)
-
+    await getResult(data,evt.target.getAttribute('value'))
     return nextQuestion + 1 !== data.length ? setNextQuestion( nextQuestion += 1) : null;
   }
+
+  const getResult = (data, answer) => {
+      data[nextQuestion].map( item => {
+      if( item.correct_answer === answer) {
+        setCorrectAnswer(correctAnswer += 1);
+      }
+    })
+  } 
 
   const getData = async() => {
     const res = await fetch(`http://localhost:3000/generalknowledge`);
@@ -25,6 +32,7 @@ function Quiz ({ sideBarState, category }) {
   useEffect(() => {
     getData()
   },[]);
+  console.log(correctAnswer,  nextQuestion)
   return (
     <div className={ sideBarState ? 'quiz-wraper-open': 'quiz-wraper-close' }>
       {data.length === 0 ? <h3>{loading}</h3>:
@@ -33,7 +41,9 @@ function Quiz ({ sideBarState, category }) {
           <h4>{data[nextQuestion].map(item => item.question)}</h4>
         </div>
         <div className="answers"> 
-          {data[nextQuestion].map(item => item.answers.map(item => <input type="submit"  onClick={ getNextQuestion}  value={item} /> ))}
+          {data[nextQuestion].map(item => <div >
+            {item.answers.map(item => <input type="submit" value={item } onClick={ getNextQuestion } /> )}
+          </div> )}
         </div>
       </div>
       }
