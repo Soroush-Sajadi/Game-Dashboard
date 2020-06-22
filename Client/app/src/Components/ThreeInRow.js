573b15ad68a3ed48fbe111282f69ae98b37975b0
Soroush-Sajadi <soorooshsadjadee@gmail.com>
2020-06-22 17:19:44 +0200

    three in row

--- Client/app/src/Components/ThreeInRow.js
+++ Client/app/src/Components/ThreeInRow.js
@@ -23,12 +23,18 @@ console.log(data)
   return (
     <div className={ sideBarState ? 'threeInRow-wraper-open': 'threeInRow-wraper-close' }>
       {data.length === 0 ?loading :
+      <>
+      <div className="side-div">
+        <h3 className={turn % 2 === 0 ? "active" : "your-turn"}>You</h3>
+        <h3 className={turn % 2 !== 0 ? "active" : "pc-turn"}>computer</h3>
+        </div>
       <div className="three-in-row-wrapper">
         {data.map( (item, i) => 
           <div key={i} id={item.id} className="three-in-row" onClick={play}>
           </div>
         )}
       </div>
+      </>
       }
     </div>
   )
